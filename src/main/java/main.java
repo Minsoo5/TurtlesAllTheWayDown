f5bd7cc895ecebdc12fc5acb2c9ab21d74463f73
Minsoo Jo <134647176+Minsoo5@users.noreply.github.com>
2023-08-04 19:56:34 -0400

    Timers Tested

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -4,11 +4,18 @@ import java.time.Instant;
 public class main {
     public static void main(String[] args) {
 
-        Instant start = Instant.now();
-        GCD.findGCD();
-        Instant finish = Instant.now();
-        long timeElapsed = Duration.between(start, finish).toMillis();
+        Instant start1 = Instant.now();
+        GCD.findGCD(180, 378);
+        Instant finish1 = Instant.now();
+        long timeElapsed1 = Duration.between(start1, finish1).toNanos();
 
+        Instant start2 = Instant.now();
+        GCD2.findGCD2(180, 378);
+        Instant finish2 = Instant.now();
+        long timeElapsed2 = Duration.between(start2, finish2).toNanos();
+
+        System.out.println("Recursion Time:       " + timeElapsed1);
+        System.out.println("Non-Recustion Time:   " + timeElapsed2);
 
 
     }
